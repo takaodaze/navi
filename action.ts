18bf421e85daf015d6783ef35db2aa1c337c3d4b
takaodaze <gin2798@gmail.com>
2022-05-28 16:04:31 +0900

    impl

--- action.ts
+++ action.ts
@@ -3,3 +3,29 @@ export const mainCommandHandler = () => {
 
   console.log("何をしますか");
 };
+
+type Menu = "hoge" | "foo";
+const menuList: Menu[] = ["hoge", "foo"];
+
+const printMenuText = (selector: number) => {
+  const maxMenuLength = menuList.reduce((prev, curr) => {
+    const menuLen = curr.length;
+    return Math.max(prev, menuLen);
+  }, 0);
+
+  const menuText = menuList.reduce((prev, curr, idx) => {
+    const spaces = new Array(maxMenuLength - curr.length)
+      .fill(0)
+      .reduce((p, _) => p + " ", "");
+    const surfix = idx === selector ? " <-" : "";
+    const menuLine = `${curr}${spaces}${surfix}\n`;
+    return prev + menuLine;
+  }, "");
+
+  return menuText;
+};
+
+Deno.test({ name: "printMenuText" }, () => {
+  const text = printMenuText(1);
+  console.log(text);
+});
