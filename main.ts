acc5001130562333f94b5eb258ffb6b760a5ffa0
takaodaze <gin2798@gmail.com>
2022-05-28 14:47:51 +0900

    declare task

--- main.ts
+++ main.ts
@@ -1,13 +1,4 @@
 import { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
-import { sleep } from "./util/sleep.ts";
+import { action } from "./action.ts";
 
-await new Command()
-  .name("navi")
-  .action(async () => {
-    console.log("start navi");
-    while (true) {
-      await sleep(1000);
-      console.log("in loop");
-    }
-  })
-  .parse(Deno.args);
+await new Command().name("navi").action(action).parse(Deno.args);
---
+++ test.ts
@@ -0,0 +1,4 @@
+import { expect } from "https://deno.land/x/expect@v0.2.9/mod.ts";
+Deno.test("sample test", () => {
+  expect(2).toBe(2);
+});
